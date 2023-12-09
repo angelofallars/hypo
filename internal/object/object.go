05e2d8e387910651f93a5a61b49831aaaabab62f
Angelo Fallaria <ba.fallaria@gmail.com>
2023-12-10 06:15:55 +0800

    refactor(obj): rename type constants to <type>Type

--- internal/object/object.go
+++ internal/object/object.go
@@ -10,12 +10,12 @@ import (
 type ObjectType string
 
 const (
-	NumberObject ObjectType = "Number"
-	StringObject ObjectType = "String"
-	BoolObject   ObjectType = "Bool"
-	ObjObject    ObjectType = "Obj"
-	NullObject   ObjectType = "Null"
-	ArrayObject  ObjectType = "Array"
+	NumberType ObjectType = "Number"
+	StringType ObjectType = "String"
+	BoolType   ObjectType = "Bool"
+	ObjType    ObjectType = "Obj"
+	NullType   ObjectType = "Null"
+	ArrayType  ObjectType = "Array"
 )
 
 // Dummy method to make the type enum-like.
@@ -33,33 +33,33 @@ type Number struct {
 	Value float64
 }
 
-func (n *Number) Type() ObjectType { return NumberObject }
+func (n *Number) Type() ObjectType { return NumberType }
 func (n *Number) String() string   { return fmt.Sprint(n.Value) }
 
 type String struct {
 	Value string
 }
 
-func (s *String) Type() ObjectType { return StringObject }
+func (s *String) Type() ObjectType { return StringType }
 func (s *String) String() string   { return "\"" + s.Value + "\"" }
 
 type Bool struct {
 	Value bool
 }
 
-func (b *Bool) Type() ObjectType { return BoolObject }
+func (b *Bool) Type() ObjectType { return BoolType }
 func (b *Bool) String() string   { return fmt.Sprint(b.Value) }
 
 type Null struct{}
 
-func (n *Null) Type() ObjectType { return NullObject }
+func (n *Null) Type() ObjectType { return NullType }
 func (n *Null) String() string   { return "null" }
 
 type Array struct {
 	Value []Object
 }
 
-func (n *Array) Type() ObjectType { return ArrayObject }
+func (n *Array) Type() ObjectType { return ArrayType }
 func (n *Array) String() string {
 	displays := sliceutil.Map(n.Value,
 		func(obj Object) string { return obj.String() },
